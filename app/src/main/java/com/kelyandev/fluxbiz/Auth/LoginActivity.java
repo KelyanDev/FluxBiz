b382d237ea06c3a3947a36829430f8f1c62469a6
KelyanDev <therealmortem@gmail.com>
2024-10-11 12:00:56 +0200

    Authentification update

--- app/src/main/java/com/kelyandev/fluxbiz/Auth/LoginActivity.java
+++ app/src/main/java/com/kelyandev/fluxbiz/Auth/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.kelyandev.fluxbiz.Auth;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -9,12 +12,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.kelyandev.fluxbiz.MainActivity;
 import com.kelyandev.fluxbiz.R;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText editTextEmail, editTextPassword;
+    private Button loginButton;
     private TextView viewRegister;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +38,43 @@ public class LoginActivity extends AppCompatActivity {
             return insets;
         });
 
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
         viewRegister = findViewById(R.id.textViewRegister);
+        loginButton = findViewById(R.id.buttonLogin);
+        mAuth = FirebaseAuth.getInstance();
 
         viewRegister.setOnClickListener( view -> {
             startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+        });
+
+        loginButton.setOnClickListener(view -> loginUser());
+    }
+
+    private void loginUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            editTextEmail.setError("Votre email est requise pour vous connecter.");
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            editTextPassword.setError("Vous devez indiquer votre mot de passe");
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                FirebaseUser user = mAuth.getCurrentUser();
+                if (user != null) {
+                    Toast.makeText(LoginActivity.this, "Connexion réussie", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Connexion échouée" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
--- app/src/main/java/com/kelyandev/fluxbiz/Auth/RegisterActivity.java
+++ app/src/main/java/com/kelyandev/fluxbiz/Auth/RegisterActivity.java
@@ -1,6 +1,9 @@
 package com.kelyandev.fluxbiz.Auth;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -9,12 +12,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.kelyandev.fluxbiz.R;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private EditText editTextUsername, editTextEmail, editTextPassword;
+    private Button registerButton;
     private TextView viewLogin;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +37,45 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
         viewLogin = findViewById(R.id.textViewLogin);
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        registerButton = findViewById(R.id.buttonRegister);
+
+        mAuth = FirebaseAuth.getInstance();
 
         viewLogin.setOnClickListener( view -> {
             startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+        });
+
+        registerButton.setOnClickListener(view -> registerUser());
+    }
+
+    private void registerUser() {
+        String username = editTextUsername.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String email = editTextEmail.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            editTextEmail.setError("Votre email est requise");
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            editTextPassword.setError("Le mot de passe est requis");
+            return;
+        }
+        if (TextUtils.isEmpty(username)) {
+            editTextUsername.setError("Votre nom d'utilisateur est requis");
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Toast.makeText(RegisterActivity.this, "Votre compte a bien été créé", Toast.LENGTH_SHORT).show();
+                finish();
+            } else {
+                Toast.makeText(RegisterActivity.this, "Erreur lors de la création du compte :" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
