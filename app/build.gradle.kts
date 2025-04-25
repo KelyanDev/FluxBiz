99b107af701eba119c41315873b2264b3a4f5b31
KelyanDev <therealmortem@gmail.com>
2025-04-25 15:17:13 +0200

    Faster user verification, ability to disconnect specific devices

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 3
-        versionName = "2.0.0"
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
